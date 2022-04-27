a699521b68e6b6e49fecf4ec39352c4a3ef5c3f5
cyman <cy@bungus.one>
2022-04-27 10:25:37 -0700

    tweaks

--- Makefile
+++ Makefile
@@ -2,14 +2,11 @@ SRC = test/hello.c test/plusplus.cpp
 
 .PHONY: all
 ifdef PROFILE
-BUILDDIR := build/$(PROFILE)
 ifeq ($(PROFILE),debug)
     CFLAGS := -g
-    EXE_PREFIX := $(BUILDDIR)/db_
 else ifeq ($(PROFILE),release)
     CFLAGS := -O2
     LDFLAGS := -s
-    EXE_PREFIX := $(BUILDDIR)/r_
 else
     $(error Invalid profile: $(PROFILE))
 endif
@@ -19,28 +16,28 @@ CFLAGS += -std=gnu99
 EXE := $(addprefix $(EXE_PREFIX),$(basename $(notdir $(SRC))))
 
 all: $(EXE)
-	$(info $(PROFILE) build is complete.)
+	@echo "$(PROFILE) build is complete."
 
 include magic.mk
 
-$(EXE): $(EXE_PREFIX)%: $(BUILDDIR)/%.o
+$(EXE): $(EXE_PREFIX)%: $(OBJDIR)/%.o
 	$(CC) $^ $(LDFLAGS) -o $@
-else
-    ifeq ($(MAKECMDGOALS),clean)
+else # PROFILE
+ifeq ($(MAKECMDGOALS),clean)
     MAKECMDGOALS :=
     TARGET := clean
-    else
-        ifeq (,$(filter-out all,$(MAKECMDGOALS)))
-            MAKECMDGOALS := debug
-        endif
-        TARGET := $(MAKECMDGOALS)
+else
+    ifeq (,$(filter-out all,$(MAKECMDGOALS)))
+        MAKECMDGOALS := debug
     endif
+    TARGET := $(MAKECMDGOALS)
+endif
 .PHONY: $(TARGET)
 all: $(TARGET)
 
 clean:
-	rm -rf build/**/*.o build/**/*.d
+	rm -rf `find build/ -name "*.[od]"`
 
 $(MAKECMDGOALS):
 	@$(MAKE) PROFILE=$(MAKECMDGOALS) --no-print-directory
-endif
+endif # PROFILE
--- magic.mk
+++ magic.mk
@@ -1,29 +1,29 @@
-BUILDDIR ?= build/debug
-DEPFILE ?= $(BUILDDIR)/deps.d
+ifndef SRC
+    $(error SRC must be defined.)
+endif
+
+OBJDIR := build/$(PROFILE)
+DEPFILE ?= build/deps.d
 DEPGENFLAGS ?= -MM
 
-ifeq ($(flavor SRC),undefined)
-    $(error SRC must be defined)
-else
-    getobjpath = $(addprefix $(BUILDDIR)/,$(addsuffix .o,$(basename $(notdir $(1)))))
-    OBJ = $(call getobjpath,$(SRC))
-endif
+getobjpath = $(addprefix $(OBJDIR)/,$(addsuffix .o,$(basename $(notdir $(1)))))
+OBJ = $(call getobjpath,$(SRC))
 
 define \n
 
 
 endef
 
-$(BUILDDIR):
-	mkdir -p $(BUILDDIR)
+$(OBJDIR):
+	mkdir -p $(OBJDIR)
 
-SRC_C := $(filter %.c,$(SRC))
-SRC_CXX := $(filter %.cpp,$(SRC))
+DEPGEN.c = $(CC) $(CFLAGS) $(DEPGENFLAGS)
+DEPGEN.cpp = $(CXX) $(CXXFLAGS) $(DEPGENFLAGS)
+DEPGEN.cc = $(DEPGEN.cpp)
 
-$(DEPFILE): $(MAKEFILE_LIST) | $(BUILDDIR)
+$(DEPFILE): $(MAKEFILE_LIST) | $(OBJDIR)
 	> $(DEPFILE)
-	$(foreach f,$(SRC_C),$(CC) $(CFLAGS) $(DEPGENFLAGS) -MT '$(call getobjpath,$(f)) $(DEPFILE)' $(f) >> $(DEPFILE)$(\n))
-	$(foreach f,$(SRC_CXX),$(CC) $(CXXFLAGS) $(DEPGENFLAGS) -MT '$(call getobjpath,$(f)) $(DEPFILE)' $(f) >> $(DEPFILE)$(\n))
+	$(foreach f,$(SRC),$(DEPGEN$(suffix $(f))) -MT '$(call getobjpath,$(f)) $(DEPFILE)' $(f) >> $(DEPFILE)$(\n))
 
 $(OBJ):
 	$(COMPILE$(suffix $<)) $< $(OUTPUT_OPTION)
